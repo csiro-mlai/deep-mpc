a62bd2735c6b2adc9608719fd824a55f5651df7c
Marcel Keller <mks.keller@gmail.com>
2023-01-05 22:45:00 +1100

    Update to fix bugs in max-pooling.

--- Dockerfile
+++ Dockerfile
@@ -16,7 +16,7 @@ ADD prepare.py .
 RUN ./prepare.py
 
 RUN git clone https://github.com/data61/MP-SPDZ
-RUN cd MP-SPDZ; git checkout 0f7020d791a667ede375aa365f109ac286e89d43
+RUN cd MP-SPDZ; git checkout a3acf6e8a304ae996dc8624c6315e9d9e0b79107
 
 ADD build-mp-spdz.sh .
 RUN ./build-mp-spdz.sh
--- build-mp-spdz.sh
+++ build-mp-spdz.sh
@@ -3,7 +3,6 @@
 cd MP-SPDZ || exit 1
 echo CXX = clang++ >> CONFIG.mine
 echo MY_CFLAGS += -DCHOP_MEMORY >> CONFIG.mine
-git clone https://github.com/wbhart/mpir
-make -j8 tldr
+make cmake boost mpir
 mkdir static
 make -j8 {static/,}{{{replicated,sy-rep,rep4}-ring,{t,h}emi,atlas}-party,emulate}.x
